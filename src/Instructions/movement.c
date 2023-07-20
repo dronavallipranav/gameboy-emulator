ba4f716b192d2be986bc22e30b8a289a7966d1c7
Pranav Dronavalli <dronavallipranav@gmail.com>
2023-07-20 11:36:21 -0500

    nearly finished opcodes, created loadIntoMem and added documentation

--- src/Instructions/movement.c
+++ src/Instructions/movement.c
@@ -1,13 +1,32 @@
 #include <movement.h>
 #include <stdbool.h>
 #include <stdio.h>
+ /*
+ This function loads an immediate 8 bit value into specified register using function pointer
+ @param: imm - 8-bit immediate value
+ */
  void loadImm(Z80_State *cpu, void (*setReg)(Z80_State *, uint8_t), uint8_t imm){
     setReg(cpu,imm);
  }
- void loadMem(Z80_State *cpu, void (*setReg)(Z80_State *, uint8_t), uint16_t offset){
+  /*
+ This function uses a function pointer to generalize loading a value from memory into a register
+ @param: offset - 16-bit register offset specified by opcode
+ */
+ void loadFromMem(Z80_State *cpu, void (*setReg)(Z80_State *, uint8_t), uint16_t offset){
     uint8_t val = cpu -> memory[offset];
     setReg(cpu, val);
  }
+ /*
+ This function uses a function pointer to generalize loading a register into memory
+ @param: offset - 16-bit register offset specified by opcode
+ */
+ void loadIntoMem(Z80_State *cpu, uint8_t(*getReg)(Z80_State *), uint16_t offset){
+    cpu -> memory[offset] = getReg;
+ }
+  /*
+ This function handles all 8-bit loads
+ @param: opcode - code to specify which load instruction
+ */
  void loadReg(Z80_State *cpu, uint8_t opcode){
 
     uint8_t (*getReg)(Z80_State *);
@@ -45,7 +64,7 @@
         break;
         case 0x7E:
         setReg = cpu->setA;
-        loadMem(cpu, setReg, cpu->HL_pair);
+        loadFromMem(cpu, setReg, cpu->HL_pair);
         status = false;
         break;
 
@@ -75,7 +94,7 @@
         break;
         case 0x46:
         setReg = cpu->setB;
-        loadMem(cpu, setReg, cpu->HL_pair);
+        loadFromMem(cpu, setReg, cpu->HL_pair);
         status = false;
         break;
 
@@ -105,7 +124,127 @@
         break;
         case 0x4E:
         setReg = cpu->setC;
-        loadMem(cpu, setReg, cpu->HL_pair);
+        loadFromMem(cpu, setReg, cpu->HL_pair);
+        status = false;
+        break;
+
+        case 0x50:
+        setReg = cpu->setD;  
+        getReg = cpu->getB;
+        break;
+        case 0x51:
+        setReg = cpu->setD;  
+        getReg = cpu->getC;
+        break;
+        case 0x52:
+        setReg = cpu->setD;  
+        getReg = cpu->getD;
+        break;
+        case 0x53:
+        setReg = cpu->setD;  
+        getReg = cpu->getE;
+        break;
+        case 0x54:
+        setReg = cpu->setD;  
+        getReg = cpu->getH;
+        break;
+        case 0x55:
+        setReg = cpu->setD;  
+        getReg = cpu->getL;
+        break;
+        case 0x56:
+        setReg = cpu->setD;
+        loadFromMem(cpu, setReg, cpu->HL_pair);
+        status = false;
+        break;
+
+        case 0x58:
+        setReg = cpu->setE;  
+        getReg = cpu->getB;
+        break;
+        case 0x59:
+        setReg = cpu->setE;  
+        getReg = cpu->getC;
+        break;
+        case 0x5A:
+        setReg = cpu->setE;  
+        getReg = cpu->getD;
+        break;
+        case 0x5B:
+        setReg = cpu->setE;  
+        getReg = cpu->getE;
+        break;
+        case 0x5C:
+        setReg = cpu->setE;  
+        getReg = cpu->getH;
+        break;
+        case 0x5D:
+        setReg = cpu->setE;  
+        getReg = cpu->getL;
+        break;
+        case 0x5E:
+        setReg = cpu->setE;
+        loadFromMem(cpu, setReg, cpu->HL_pair);
+        status = false;
+        break;
+
+        case 0x60:
+        setReg = cpu->setH;  
+        getReg = cpu->getB;
+        break;
+        case 0x61:
+        setReg = cpu->setH;  
+        getReg = cpu->getC;
+        break;
+        case 0x62:
+        setReg = cpu->setH;  
+        getReg = cpu->getD;
+        break;
+        case 0x63:
+        setReg = cpu->setH;  
+        getReg = cpu->getE;
+        break;
+        case 0x64:
+        setReg = cpu->setH;  
+        getReg = cpu->getH;
+        break;
+        case 0x65:
+        setReg = cpu->setH;  
+        getReg = cpu->getL;
+        break;
+        case 0x66:
+        setReg = cpu->setH;
+        loadFromMem(cpu, setReg, cpu->HL_pair);
+        status = false;
+        break;
+
+        case 0x68:
+        setReg = cpu->setL;  
+        getReg = cpu->getB;
+        break;
+        case 0x69:
+        setReg = cpu->setL;  
+        getReg = cpu->getC;
+        break;
+        case 0x6A:
+        setReg = cpu->setL;  
+        getReg = cpu->getD;
+        break;
+        case 0x6B:
+        setReg = cpu->setL;  
+        getReg = cpu->getE;
+        break;
+        case 0x6C:
+        setReg = cpu->setL;  
+        getReg = cpu->getH;
+        break;
+        case 0x6D:
+        setReg = cpu->setL;  
+        getReg = cpu->getL;
+        break;
+        case 0x6E:
+        setReg = cpu->setL;
+        loadFromMem(cpu, setReg, cpu->HL_pair);
         status = false;
         break;
 
