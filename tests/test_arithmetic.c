f1c153aa677bc6715c85714955a49c8050aad227
Pranav Dronavalli <dronavallipranav@gmail.com>
2023-07-25 15:08:46 -0500

    added tests for rest of 8-bit alu ops, need to add more flag checks

--- tests/test_arithmetic.c
+++ tests/test_arithmetic.c
@@ -68,8 +68,72 @@ void test_subImm() {
     
 }
 
+void test_and8() {
 
+    Z80_State cpu;
+    initCPU(&cpu);
+    cpu.AF.A = 0xF0;
+    cpu.BC.B = 0xFF;
+    and8(&cpu, cpu.BC.B);
+    CU_ASSERT_EQUAL(cpu.AF.A, 0xF0);
+    CU_ASSERT_TRUE(cpu.AF.flags.Z == ((cpu.AF.A == 0) ? 1 : 0));
+    CU_ASSERT_TRUE(cpu.AF.flags.N == 0);
+    CU_ASSERT_TRUE(cpu.AF.flags.H == 1);
+    CU_ASSERT_TRUE(cpu.AF.flags.C == 0);
+    
+}
 
+void test_or8() {
+    
+    Z80_State cpu;
+    initCPU(&cpu);
+    cpu.AF.A = 0xF0;
+    cpu.BC.B = 0x0F;
+    or8(&cpu, cpu.BC.B);
+    CU_ASSERT_EQUAL(cpu.AF.A, 0xFF);
+    
+}
+
+void test_xor8() {
+    
+    Z80_State cpu;
+    initCPU(&cpu);
+    cpu.AF.A = 0xAA;
+    cpu.BC.B = 0xFF;
+    xor8(&cpu, cpu.BC.B);
+    CU_ASSERT_EQUAL(cpu.AF.A, 0x55);
+}
+
+void test_cp8() {
+
+    Z80_State cpu;
+    initCPU(&cpu);
+    cpu.AF.A = 10;
+    cpu.BC.B = 5;
+    cp8(&cpu, cpu.BC.B);
+    CU_ASSERT_EQUAL(cpu.AF.flags.Z, 0);
+    
+}
+
+void test_inc8() {
+    
+    Z80_State cpu;
+    initCPU(&cpu);
+    cpu.AF.A = 9;
+    inc8(&cpu, cpu.setA, cpu.AF.A);
+    CU_ASSERT_EQUAL(cpu.AF.A, 10);
+    
+}
+
+void test_dec8() {
+    
+    Z80_State cpu;
+    initCPU(&cpu);
+    cpu.AF.A = 10;
+    dec8(&cpu, cpu.setA, cpu.AF.A);
+    CU_ASSERT_EQUAL(cpu.AF.A, 9);
+    
+}
 
 int add_arithmetic_tests(CU_pSuite suite) {
     if (NULL == CU_add_test(suite, "Test Add8", test_add8)) {
@@ -96,5 +160,28 @@ int add_arithmetic_tests(CU_pSuite suite) {
         return -1; 
     }
 
+    if (NULL == CU_add_test(suite, "Test And8", test_and8)) {
+        return -1; 
+    }
+
+    if (NULL == CU_add_test(suite, "Test or8", test_or8)) {
+        return -1; 
+    }
+
+    if (NULL == CU_add_test(suite, "Test xor8", test_xor8)) {
+        return -1; 
+    }
+
+    if (NULL == CU_add_test(suite, "Test cp8", test_cp8)) {
+        return -1; 
+    }
+
+    if (NULL == CU_add_test(suite, "Test inc8", test_inc8)) {
+        return -1; 
+    }
+
+    if (NULL == CU_add_test(suite, "Test dec8", test_dec8)) {
+        return -1; 
+    }
     return 0;
 }
