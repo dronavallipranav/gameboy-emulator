779b41272b7a1add41bd7a0e8e73141d590b351c
Pranav Dronavalli <dronavallipranav@gmail.com>
2023-08-09 23:06:27 -0500

    changed handler name

--- tests/test_extended.c
+++ tests/test_extended.c
@@ -5,7 +5,7 @@ void test_swap() {
     Z80_State cpu;
     initCPU(&cpu);
     cpu.AF.A = 0x53;
-    handler_cp(&cpu, 0x37);
+    handler_cb(&cpu, 0x37);
     CU_ASSERT_EQUAL(cpu.AF.A, 0x35);
     CU_ASSERT_EQUAL(cpu.AF.flags.Z, 0); 
 }
