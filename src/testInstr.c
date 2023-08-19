12a345d9a378e69e57a27415ee18f45707806a63
Pranav Dronavalli <dronavallipranav@gmail.com>
2023-08-19 13:54:26 -0500

    test now seperately loads cart mem

--- src/testInstr.c
+++ src/testInstr.c
@@ -6,7 +6,7 @@ int main()
 {
     Z80_State cpu;
     initCPU(&cpu);
-    load_memory(cpu.mmu, "/Users/pranavdronavalli/Downloads/gb-test-roms-master/cpu_instrs/individual/01-special.gb");
+    load_memory(cpu.mmu, "/Users/pranavdronavalli/Downloads/gb-test-roms-master/cpu_instrs/individual/03-op sp,hl.gb");
     init_opcode_table();
     execute_cycle(&cpu);
 }
